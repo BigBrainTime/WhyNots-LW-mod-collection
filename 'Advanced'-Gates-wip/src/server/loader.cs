3afc68e0e4733c83e4549ea4dc0b8f88dfc33e1d
BigBrainTime <57951435+BigBrainTime@users.noreply.github.com>
2021-12-07 17:01:38 -0600

    added base folder structure

--- 'Advanced'-Gates-wip/src/server/loader.cs
+++ 'Advanced'-Gates-wip/src/server/loader.cs
@@ -5,13 +5,13 @@ using System;
 using LogicWorld.Server.Circuitry;
 using STF.Server;
 
-namespace STF.Server
+namespace STFAG.Server
 {
     public class Loader : ServerMod
     {
         protected override void Initialize()
         {
-            Logger.Info("Basic Gates initialized");
+            Logger.Info("Advanced Gates initialized");
         }
     }
 }
